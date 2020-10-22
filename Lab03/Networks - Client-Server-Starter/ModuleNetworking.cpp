3380c3681cfe4f9b7a44aa8e7503d62623388f31
vsRushy <vsRushy@users.noreply.github.com>
2020-10-22 08:41:41 +0200

    Added lab3 base code.

---
+++ Lab03/Networks - Client-Server-Starter/ModuleNetworking.cpp
@@ -0,0 +1,109 @@
+#include "Networks.h"
+#include "ModuleNetworking.h"
+
+
+static uint8 NumModulesUsingWinsock = 0;
+
+
+
+void ModuleNetworking::reportError(const char* inOperationDesc)
+{
+	LPVOID lpMsgBuf;
+	DWORD errorNum = WSAGetLastError();
+
+	FormatMessage(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER |
+		FORMAT_MESSAGE_FROM_SYSTEM |
+		FORMAT_MESSAGE_IGNORE_INSERTS,
+		NULL,
+		errorNum,
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(LPTSTR)&lpMsgBuf,
+		0, NULL);
+
+	ELOG("Error %s: %d- %s", inOperationDesc, errorNum, lpMsgBuf);
+}
+
+void ModuleNetworking::disconnect()
+{
+	for (SOCKET socket : sockets)
+	{
+		shutdown(socket, 2);
+		closesocket(socket);
+	}
+
+	sockets.clear();
+}
+
+bool ModuleNetworking::init()
+{
+	if (NumModulesUsingWinsock == 0)
+	{
+		NumModulesUsingWinsock++;
+
+		WORD version = MAKEWORD(2, 2);
+		WSADATA data;
+		if (WSAStartup(version, &data) != 0)
+		{
+			reportError("ModuleNetworking::init() - WSAStartup");
+			return false;
+		}
+	}
+
+	return true;
+}
+
+bool ModuleNetworking::preUpdate()
+{
+	if (sockets.empty()) return true;
+
+	// NOTE(jesus): You can use this temporary buffer to store data from recv()
+	const uint32 incomingDataBufferSize = Kilobytes(1);
+	byte incomingDataBuffer[incomingDataBufferSize];
+
+	// TODO(jesus): select those sockets that have a read operation available
+
+	// TODO(jesus): for those sockets selected, check wheter or not they are
+	// a listen socket or a standard socket and perform the corresponding
+	// operation (accept() an incoming connection or recv() incoming data,
+	// respectively).
+	// On accept() success, communicate the new connected socket to the
+	// subclass (use the callback onSocketConnected()), and add the new
+	// connected socket to the managed list of sockets.
+	// On recv() success, communicate the incoming data received to the
+	// subclass (use the callback onSocketReceivedData()).
+
+	// TODO(jesus): handle disconnections. Remember that a socket has been
+	// disconnected from its remote end either when recv() returned 0,
+	// or when it generated some errors such as ECONNRESET.
+	// Communicate detected disconnections to the subclass using the callback
+	// onSocketDisconnected().
+
+	// TODO(jesus): Finally, remove all disconnected sockets from the list
+	// of managed sockets.
+
+	return true;
+}
+
+bool ModuleNetworking::cleanUp()
+{
+	disconnect();
+
+	NumModulesUsingWinsock--;
+	if (NumModulesUsingWinsock == 0)
+	{
+
+		if (WSACleanup() != 0)
+		{
+			reportError("ModuleNetworking::cleanUp() - WSACleanup");
+			return false;
+		}
+	}
+
+	return true;
+}
+
+void ModuleNetworking::addSocket(SOCKET socket)
+{
+	sockets.push_back(socket);
+}
---
+++ Lab03/Networks - Client-Server-Starter/ModuleNetworkingClient.cpp
@@ -0,0 +1,63 @@
+#include "ModuleNetworkingClient.h"
+
+
+bool  ModuleNetworkingClient::start(const char * serverAddressStr, int serverPort, const char *pplayerName)
+{
+	playerName = pplayerName;
+
+	// TODO(jesus): TCP connection stuff
+	// - Create the socket
+	// - Create the remote address object
+	// - Connect to the remote address
+	// - Add the created socket to the managed list of sockets using addSocket()
+
+	// If everything was ok... change the state
+	state = ClientState::Start;
+
+	return true;
+}
+
+bool ModuleNetworkingClient::isRunning() const
+{
+	return state != ClientState::Stopped;
+}
+
+bool ModuleNetworkingClient::update()
+{
+	if (state == ClientState::Start)
+	{
+		// TODO(jesus): Send the player name to the server
+	}
+
+	return true;
+}
+
+bool ModuleNetworkingClient::gui()
+{
+	if (state != ClientState::Stopped)
+	{
+		// NOTE(jesus): You can put ImGui code here for debugging purposes
+		ImGui::Begin("Client Window");
+
+		Texture *tex = App->modResources->client;
+		ImVec2 texSize(400.0f, 400.0f * tex->height / tex->width);
+		ImGui::Image(tex->shaderResource, texSize);
+
+		ImGui::Text("%s connected to the server...", playerName.c_str());
+
+		ImGui::End();
+	}
+
+	return true;
+}
+
+void ModuleNetworkingClient::onSocketReceivedData(SOCKET socket, byte * data)
+{
+	state = ClientState::Stopped;
+}
+
+void ModuleNetworkingClient::onSocketDisconnected(SOCKET socket)
+{
+	state = ClientState::Stopped;
+}
+
