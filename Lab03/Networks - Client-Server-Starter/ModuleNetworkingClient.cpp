ca6a357ab7647ea0f2074275dc73b5e88cbe9669
vsRushy <vsRushy@users.noreply.github.com>
2020-10-26 21:41:08 +0100

    Fixed errors.

--- Lab03/Networks - Client-Server-Starter/ModuleNetworkingClient.cpp
+++ Lab03/Networks - Client-Server-Starter/ModuleNetworkingClient.cpp
@@ -47,7 +47,7 @@ bool ModuleNetworkingClient::update()
 	if (state == ClientState::Start)
 	{
 		// TODO(jesus): Send the player name to the server
-		if (send(s, (playerName + '\0').c_str(), playerName.size() + 1, 0))
+		if (send(s, (playerName + '\0').c_str(), playerName.size() + 1, 0) == SOCKET_ERROR)
 		{
 			reportError("Can't send.");
 		}
