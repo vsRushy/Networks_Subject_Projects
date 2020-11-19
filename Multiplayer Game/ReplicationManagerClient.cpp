74fdffd4cb72a3d7cbacfa5bd46339205782e393
vsRushy <vsRushy@users.noreply.github.com>
2020-11-19 09:18:07 +0100

    Skeleton of world replication classes.

--- Multiplayer Game/ReplicationManagerClient.cpp
+++ Multiplayer Game/ReplicationManagerClient.cpp
@@ -1,3 +1,9 @@
 #include "Networks.h"
+#include "ReplicationManagerClient.h"
 
 // TODO(you): World state replication lab session
+
+void ReplicationManagerClient::read(const InputMemoryStream& packet)
+{
+
+}
--- Multiplayer Game/ReplicationManagerServer.cpp
+++ Multiplayer Game/ReplicationManagerServer.cpp
@@ -1,3 +1,23 @@
 #include "Networks.h"
+#include "ReplicationManagerServer.h"
 
 // TODO(you): World state replication lab session
+void ReplicationManagerServer::create(uint32 networkId)
+{
+
+}
+
+void ReplicationManagerServer::update(uint32 networkId)
+{
+
+}
+
+void ReplicationManagerServer::destroy(uint32 networkId)
+{
+
+}
+
+void ReplicationManagerServer::write(OutputMemoryStream& packet)
+{
+
+}
