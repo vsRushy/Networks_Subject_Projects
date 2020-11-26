f0a0a45cd1f59a374562648010d159b5ee26466f
thedoctormarc <36292102+thedoctormarc@users.noreply.github.com>
2020-11-26 11:28:14 +0100

    Update DeliveryManager.h

--- Multiplayer Game/DeliveryManager.h
+++ Multiplayer Game/DeliveryManager.h
@@ -1,25 +1,6 @@
 #pragma once
 
 // TODO(you): Reliability on top of UDP lab session
-
-class DeliveryManager
-{
-public:
-	// For senders to write a new seq. numbers into a packet Delivery * writeSequenceNu*ber(OutputMemoryStream fcpacket);
-	// For receivers to process the seq. number from an incoming packet bool processSequenceNumber(const InpufeiroryStream &packet);
-	// For receivers to write ack'ed seq. numbers into a packet bool hasSequenceNumbersPendingAck() const;
-	void writeSequenceNumbersPendingAck(OutputMemoryStream Apacket);
-	// For senders to process ack'ed seq. numbers from a packet void processAckdSequenceNumbers(const InputttemoryStream &packet); void processTimedOutPackets();
-	void clear();
-
-private:
-	// Prívate members (sender side)
-	// - The next outgoing sequence number // - A list of pending deliveries
-	// Prívate members (receiver side)
-	// - The next expected sequence number // - A list of sequence numbers pending ack
-
-};
-
 class DeliveryManager;
 class DeliveryDelegate
 {
@@ -28,8 +9,31 @@ public:
 	virtual void onDeliveryFailure(DeliveryManager* deliveryManager);
 	struct Delivery
 	{
-		uint32 sequenceHumber = 0; 
+		uint32 sequenceHumber = 0;
 		double dispatchTime = 0.0;
 		DeliveryDelegate* delegate = nullptr;
 	};
+};
+
+class DeliveryManager
+{
+public:
+	// For senders to write a new seq. numbers into a packet 
+	DeliveryDelegate::Delivery * writeSequenceNu*ber(OutputMemoryStream fcpacket);
+	// For receivers to process the seq. number from an incoming packet 
+	bool processSequenceNumber(const InputMemoryStream &packet);
+	// For receivers to write ack'ed seq. numbers into a packet 
+	bool hasSequenceNumbersPendingAck() const;
+	void writeSequenceNumbersPendingAck(OutputMemoryStream Apacket);
+	// For senders to process ack'ed seq. numbers from a packet 
+	void processAckdSequenceNumbers(const InputMemoryStream &packet);
+	void processTimedOutPackets();
+	void clear();
+
+private:
+	// Prívate members (sender side)
+	// - The next outgoing sequence number // - A list of pending deliveries
+	// Prívate members (receiver side)
+	// - The next expected sequence number // - A list of sequence numbers pending ack
+
 };
