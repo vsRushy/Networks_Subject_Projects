ca6cf5158d0f290f08c0665373e5e9bd5d6e6a74
vsRushy <vsRushy@users.noreply.github.com>
2020-12-24 22:26:54 +0100

    Cleaner code.

--- Multiplayer Game/DeliveryManager.h
+++ Multiplayer Game/DeliveryManager.h
@@ -21,29 +21,32 @@ class DeliveryManager
 public:
 	// For senders to write a new seq. numbers into a packet 
 	DeliveryDelegate::Delivery * writeSequenceNumber(OutputMemoryStream fcpacket);
+
 	// For receivers to process the seq. number from an incoming packet 
 	bool processSequenceNumber(uint32 packetSequenceNumber = UINT32_MAX);
+
 	// For receivers to write ack'ed seq. numbers into a packet 
 	bool hasSequenceNumbersPendingAck() const;
 	void writeSequenceNumbersPendingAck(OutputMemoryStream Apacket);
+
 	// For senders to process ack'ed seq. numbers from a packet 
 	void processAckdSequenceNumbers(const InputMemoryStream &packet);
 	void processTimedOutPackets();
+
 	void clear();
 
 private:
-	// Prívate members (sender side)
+	// Private members (sender side)
 	// - The next outgoing sequence number
 	uint32 nextOutgoingSequenceNumber = 0;
 
 	// - A list of pending deliveries
 	std::list<DeliveryDelegate::Delivery*> pendingDeliveries;
 
-	// Prívate members (receiver side)
+	// Private members (receiver side)
 	// - The next expected sequence number 
 	uint32 nextExpectedSequenceNumber = 0;
 
 	// - A list of sequence numbers pending ack
 	std::list<uint32> sequenceNumbersPendingAck;
-
 };
